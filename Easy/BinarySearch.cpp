7295dac532f99b233d6cba8dd2f33840d2f663b4
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2023-11-02 21:57:42 +0100

    added BinarySearch

---
+++ Easy/BinarySearch.cpp
@@ -0,0 +1,28 @@
+//
+// Created by Dino Kupinic on 02.11.23.
+//
+#include "vector"
+#include "cmath"
+
+using namespace std;
+
+class Solution {
+public:
+  int search(vector<int> &nums, int target) {
+    int low{0};
+    int high = nums.size();
+    do {
+      int middle = (low + (high - low) / 2);
+      int value = floor(nums[middle]);
+
+      if (value == target) {
+        return middle;
+      } else if (value > target) {
+        high = middle;
+      } else {
+        low = middle + 1;
+      }
+    } while (low < high);
+    return -1;
+  }
+};
