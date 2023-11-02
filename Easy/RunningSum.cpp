3a673c9e7a3bb2468aa59deea5bfaf6271242716
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2023-11-02 22:20:40 +0100

    added RunningSum.cpp

---
+++ Easy/RunningSum.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Dino Kupinic on 02.11.23.
+// https://leetcode.com/problems/running-sum-of-1d-array/
+#include "vector"
+
+using namespace std;
+
+class Solution {
+public:
+  vector<int> runningSum(vector<int> &nums) {
+    vector<int> solution;
+    solution.push_back(nums[0]);
+    for (int i = 1; i < nums.size(); i++) {
+      solution.push_back(nums[i] + solution[i - 1]);
+    }
+    return solution;
+  }
+};
