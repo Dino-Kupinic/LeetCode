d37af14a99ae7179004c253108c300bf0457799d
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2023-11-02 22:04:04 +0100

    added ValidAnagram

---
+++ Easy/ValidAnagram.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Dino Kupinic on 02.11.23.
+// https://leetcode.com/problems/valid-anagram/
+#include "string"
+#include "algorithm"
+
+using namespace std;
+
+class Solution {
+public:
+  bool isAnagram(string s, string t) {
+    sort(s.begin(), s.end());
+    sort(t.begin(), t.end());
+    if (s == t)
+      return true;
+    return false;
+  }
+};
