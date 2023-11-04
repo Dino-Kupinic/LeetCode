322ed909f7c2ba84badf3de7572901552801e733
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2023-11-04 22:29:34 +0100

    added BestTimeToBuyAndSellStock

---
+++ Easy/BestTimeToBuyAndSellStock.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Dino Kupinic on 04.11.23.
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+#include "vector"
+
+using namespace std;
+
+class Solution {
+public:
+  int maxProfit(vector<int>& prices) {
+    int minPrice = prices[0];
+    int profit = 0;
+    for (int i = 1; i < prices.size(); ++i) {
+      if (minPrice > prices[i]) {
+        minPrice = prices[i];
+      } else {
+        profit = max(profit, prices[i] - minPrice);
+      }
+    }
+    return profit;
+  }
+};
