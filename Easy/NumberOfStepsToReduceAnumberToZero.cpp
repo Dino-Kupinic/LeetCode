1c00d306ad859f635f28e7131f1d5aebe88fce41
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-06-01 01:50:40 +0200

    feat: add NumberOfStepsToReduceAnumberToZero

--- Easy/NumberOfStepsToReduceAnumberToZero.cpp
+++ Easy/NumberOfStepsToReduceAnumberToZero.cpp
@@ -1,10 +1,19 @@
 //
 // Created by Dino Kupinic on 01.06.24.
-//
+// https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
 
 class Solution {
 public:
-    int numberOfSteps(int num) {
-        return 1;
+    static int numberOfSteps(int num) {
+        int steps = 0;
+        while (num != 0) {
+            if (num % 2 == 0) {
+                num /= 2;
+            } else {
+                num -= 1;
+            }
+            steps++;
+        }
+        return steps;
     }
 };
