efc19c07caea204a6318a86684de2d5c9c4d69c3
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-08-21 21:13:20 +0200

    feat: add MoveZeros.cpp v2

--- Easy/MoveZeros.cpp
+++ Easy/MoveZeros.cpp
@@ -8,24 +8,39 @@ using namespace std;
 
 class Solution {
 public:
+  // Version 1
+
+  // static void moveZeroes(std::vector<int> &nums) {
+  //   if (nums.size() < 2) return;
+  //
+  //   int zeroCount = 0;
+  //   vector<int> numbers{};
+  //
+  //   for (int i = 0; i < nums.size(); i++) {
+  //     if (nums[i] == 0) {
+  //       zeroCount++;
+  //     } else {
+  //       numbers.push_back(nums[i]);
+  //     }
+  //   }
+  //
+  //   for (int i = 0; i < zeroCount; i++) {
+  //     numbers.push_back(0);
+  //   }
+  //
+  //   nums = numbers;
+  // }
+
+  // Version 2
+
   static void moveZeroes(std::vector<int> &nums) {
-    if (nums.size() < 2) return;
+    int ptr2 = 0;
 
-    int zeroCount = 0;
-    vector<int> numbers{};
-
-    for (int i = 0; i < nums.size(); i++) {
-      if (nums[i] == 0) {
-        zeroCount++;
-      } else {
-        numbers.push_back(nums[i]);
+    for (int ptr1 = 0; ptr1 < nums.size(); ptr1++) {
+      if (nums[ptr1] != 0) {
+        swap(nums[ptr2], nums[ptr1]);
+        ptr2++;
       }
     }
-
-    for (int i = 0; i < zeroCount; i++) {
-      numbers.push_back(0);
-    }
-
-    nums = numbers;
   }
 };
